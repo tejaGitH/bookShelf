0f99017ad216ed2619cb105b135aa3299f00cf09
tejaGitH <navyateja.ch@outlook.com>
2024-11-25 20:27:43 +0530

    DashboardCombined_1

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,51 +1,29 @@
 const jwt = require("jsonwebtoken");
 
-const verifyToken=(req,res,next)=>{
-    // if (req.path === '/api/login' || req.path === '/api/signup') {
-    //     return next();
-    // }
+const verifyToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
-    // console.log("authHeader",authHeader);
-    // console.log("request body",req.body);
-    
     if (!authHeader) {
-      return res.status(403).json({ message: "Access Denied, No token Provided" });
+        return res.status(403).json({ message: "Access Denied, No token Provided" });
     }
-    
-    const token=authHeader && authHeader.split(' ')[1];//extracting token from bearer header 
-    console.log("authorization token:",token);
-    if(!token){
-        return res.status(403).json({message:"Access Denied, No token Provided"});
+
+    const token = authHeader && authHeader.split(' ')[1];
+    if (!token) {
+        return res.status(403).json({ message: "Access Denied, No token Provided" });
     }
-    // const bearerToken= token.split(' ')[1];
-    jwt.verify(token, process.env.SECRET_KEY,(err, decoded)=>{
-        //console.log(token);
-        //console.log(JSON.stringify(decoded)) ;
-               //console.log(decoded);
-        // if(err){
-        //     console.log("decoded token:",decoded);
-        //     return res.status(400).json({message:"invalid token",err});
-        // }
-      //   if (err) {
-      //     console.log("Token verification error:", err);
-      //     return res.status(401).json({ message: 'Invalid token' });
-      // }
-      //console.log("Decoded token:", decoded);
+
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
         if (err) {
             if (err.name === 'TokenExpiredError') {
-              return res.status(401).json({ message: 'Token expired' });
+                return res.status(401).json({ message: 'Token expired' });
             } else if (err.name === 'JsonWebTokenError') {
-              return res.status(401).json({ message: 'Invalid token' });
+                return res.status(401).json({ message: 'Invalid token' });
             } else {
-              return res.status(500).json({ message: 'Token verification failed', err });
+                return res.status(500).json({ message: 'Token verification failed', err });
             }
-            console.log(err);
-          }
-          console.log("decoded .id:",decoded.id);  
-          console.log("decoded :",decoded); 
-          req.userId = decoded.id;
-          console.log("userId:",req.userId); 
-          next();//call to next( )allows the request to process next middleware  
-    })
-}
+        }
+        req.userId = decoded.id;
+        next();
+    });
+};
+
 module.exports = verifyToken;
